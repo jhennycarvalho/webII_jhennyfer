2a3f5fdeafd2fe39c14eee4de1d8ad2cd68e9de6
jhennycarvalho <jhennyfercristine17@gmail.com>
2026-05-26 22:00:56 -0400

    autenticação ok

--- projetoWebII/projeto_web/usuarios/urls.py
+++ projetoWebII/projeto_web/usuarios/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_view, name="home"),
+    path('login/', views.login_view, name='login'),
+    path('cadastro/', views.cadastro_view, name='cadastro'),
 ]
--- projetoWebII/projeto_web/usuarios/views.py
+++ projetoWebII/projeto_web/usuarios/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+
+
 def home_view(request):
-    return HttpResponse("<p>Olá sou a jhennyfer e essa é a pagina de login de usuarios </p>")
+    return render(request, 'usuarios/login.html')
+
+
+def cadastro_view(request):
+    return render(request, 'usuarios/cadastro.html')
+
+
+def login_view(request):
+
+    if request.method == 'GET':
+        return render(request, 'usuarios/login.html')
+
+    elif request.method == 'POST':
+
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = authenticate(
+            request,
+            username=email,
+            password=senha
+        )
+
+        if user is not None:
+            login_django(request, user)
+            return HttpResponse('login autenticado com sucesso.')
+
+        else:
+            return HttpResponse('e-mail ou senha inválidos.')
+        
+def cadastro(request):
+    if request.method == 'GET':
+        request render(request, 'usuarios/cadastro.html')
+    
+    username = request.POST.get('email')
+    email = request.POST.get('email')
+    password = request.POST.get('senha')
+    first_name = request.POST.get('nome')
